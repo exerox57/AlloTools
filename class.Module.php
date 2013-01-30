223e9ab2aa281b53fbef7ac1f110363428c651d3
Anthony PALERMO <anthonypalermo80@gmail.com>
2013-01-30 14:40:26 +0100

    moduleDAO.php et module.php

---
+++ class.Module.php
@@ -0,0 +1,66 @@
+<?php 
+
+class Content {
+	
+	private $id,
+			$title,
+			$position,
+			$module,
+			$params
+	;
+	
+	public function __construct(array $valeurs) {
+
+		foreach ($valeurs as $key => $value) {
+			$setter1 = ucfirst($key);
+			$setter2 = explode('_', $setter1);
+			$setter = '';
+			foreach($setter2 as $s) {
+				$setter .= ucfirst($s);
+			}
+			$setter = 'set'. $setter;
+		
+			if (method_exists($this, $setter))
+				$this->$setter($value);
+		}
+	}
+		
+	//ID
+	public function getId() {
+		return $this->id;
+	}
+	public function setId($id) {
+			$this->id = $id;
+	}
+	//TITLE
+	public function getTitle() {
+		return $this->title;
+	}
+	public function setTitle($Title) {
+			$this->title = $Title;
+	}
+	//POSITION
+	public function getPosition() {
+		return $this->position;
+	}
+	public function setPosition($pos) {
+			$this->position = $pos;
+	}
+	//MODULE
+	public function getModule() {
+		return $this->module;
+	}
+	public function setModule($mod) {
+			$this->module = $mod;
+	}
+	//PARAMS
+	public function getParams() {
+		return $this->params;
+	}
+	public function setParams($par) {
+			$this->params = $par;
+	}
+	
+
+}
+?>
---
+++ class.ModuleDAO.php
@@ -0,0 +1,96 @@
+<?php
+class ContentDAO extends DAO{
+	
+	public function create($object) {
+	
+	}
+	
+	//Retourne un Content spécifique
+	//OK
+	public function read($no) {
+		$ligne1 = $this->db->prepare('SELECT * FROM jml_module where id=?');
+		$ligne1->bindParam(1, $no, PDO::PARAM_INT);
+		$ligne1->execute();
+		$ligne = $ligne1->fetch(PDO::FETCH_ASSOC);
+		
+		//return $ligne;
+		return new Content($ligne);
+	}
+	
+	//Récuparation de la liste des ID Module
+	public function findAllId() {
+		$requete = $this->db->prepare('SELECT id FROM jml_module ORDER BY id');
+		$requete->execute();
+		$tableIdContent = $requete->fetchAll(PDO::FETCH_ASSOC);
+		
+		return $tableIdContent;
+	}
+	
+	//vérifie la présence ou non de l'id dans la table
+	public function exists($id) {
+		$nb1 = $this->db->prepare('SELECT COUNT(*) AS nb FROM jml_module WHERE id=?');
+		$nb1->bindParam(1, $id, PDO::PARAM_INT);
+		$nb1->execute();
+		$nb = $nb1->fetch(PDO::FETCH_ASSOC);
+		
+		return ($nb['nb'] == 1) ? true : false;
+	}
+	
+	//Fonction d'Ajout / Mise à  jour d'un Content
+	public function persist(Module $mod) {
+		// Cas d'un update
+		if($this->exists($mod->getId())) {
+			$req = $this->db->prepare('
+				UPDATE jml_module ct SET 
+				title 			  = :title,
+				position 		  = :position,
+				module			  = :module,
+				params 	 		  = :params,
+				WHERE id = :id
+			');
+		
+		}
+		// Cas d'un insert
+		else{
+			$req = $this->db->prepare('
+				INSERT INTO jml_module
+				(id, title, position, module, jml_module.module, 
+				jml_module.params)
+				VALUES
+				(:id, :title, :position, :module, :params)
+			');
+		}
+		
+		//Tableau de récupération des valeurs du param $cont
+		//qui sont insérées dans la requete
+		$creat = $cont->getCreated();
+		$creat = '"'.$creat.'"';
+		
+		$array = array(
+			'id' 				=> $cont->getId(),
+			'title' 			=> $cont->getTitle() ,
+			'position' 			=> $cont->getPosition() ,
+			'module'	 		=> $cont->getModule(	) ,
+			'params'			=> $cont->getParams() ,
+			
+		);
+		//Execution de la requete
+		return $req->execute($array);
+	}
+	
+	//Override
+	public function update($object) {
+	
+	}
+	
+	//Override
+	public function delete($object) {
+		$idSupp = $object->getId();
+		if($this->exists($idSupp)) {
+			$req = $this->db->prepare('DELETE FROM jml_module 
+										WHERE id = ?'); 
+			$req->bindParam(1, $idSupp, PDO::PARAM_INT);
+			return $req->execute();
+		}
+	}
+}
